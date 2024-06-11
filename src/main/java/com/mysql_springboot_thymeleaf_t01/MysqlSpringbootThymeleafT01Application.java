81ef254db6a002475c0ef1991a26993ada929671
chloros <chloros.lin@gmail.com>
2024-06-11 13:08:49 +0800

    @component scan

--- src/main/java/com/mysql_springboot_thymeleaf_t01/MysqlSpringbootThymeleafT01Application.java
+++ src/main/java/com/mysql_springboot_thymeleaf_t01/MysqlSpringbootThymeleafT01Application.java
@@ -2,8 +2,10 @@ package com.mysql_springboot_thymeleaf_t01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mysql_springboot_thymeleaf_t01.controller")
 public class MysqlSpringbootThymeleafT01Application {
 
 	public static void main(String[] args) {
